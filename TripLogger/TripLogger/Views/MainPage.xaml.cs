3e58d8cc1ac98f895eac2205557d5c358205e80c
Vinnnie4 <70817412+Vinnnie4@users.noreply.github.com>
2020-10-21 20:21:58 -0400

    initial checkin

---
+++ TripLogger/TripLogger/Views/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripLogger.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TripLogger.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var items = new List<TripLoggerEntry>
+            {
+                new TripLoggerEntry
+                {
+                    Title = "Washington Monument",
+                    Notes = "Amazing!",
+                    Rating = 3,
+                    Date = new DateTime(2019, 2, 5),
+                    Latitude = 38.8895,
+                    Longitude = -77.0352
+                },
+                new TripLoggerEntry
+                {
+                    Title = "Golden Gate Bridge",
+                    Notes = "Foggy, but beautiful",
+                    Rating = 5,
+                    Date = new DateTime(2019, 4, 26),
+                    Latitude = 37.8268,
+                    Longitude = -122.4798
+                }
+            };
+
+            trips.ItemsSource = items;
+        }
+    }
+}
